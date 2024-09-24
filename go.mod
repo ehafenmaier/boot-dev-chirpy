3904288180fed5258d0e60d93d8f89a510a56e5b
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-09-23 22:18:25 -0700

    Initial commit of go module and main go file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ehafenmaier/boot-dev-chirpy
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	// Create new server instance
+	srv := &http.Server {
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	// Run the server
+	log.Printf("Server started on port %s", port)
+	log.Fatal(srv.ListenAndServe())
+}
