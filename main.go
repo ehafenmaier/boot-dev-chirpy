29f95c92a78c22bfa240ed4231017c72c01c303c
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-09-23 22:31:45 -0700

    Add index html page and hnadler function for root path

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	// Create a new ServeMux
 	mux := http.NewServeMux()
 
+	// Register the handler function for the root URL pattern
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	// Create new server instance
 	srv := &http.Server {
 		Addr:    ":" + port,
