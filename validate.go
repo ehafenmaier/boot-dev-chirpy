988ed90d6b81eb7febc033ffbc1f1198d34019b3
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-10-14 17:33:07 -0700

    Update validate chirp handler to use new helper functions

--- validate.go
+++ validate.go
@@ -7,18 +7,6 @@ import (
 )
 
 func validateChirpHandler(rw http.ResponseWriter, rq *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type returnError struct {
-		Error string `json:"error"`
-	}
-
-	type returnValid struct {
-		Valid bool `json:"valid"`
-	}
-
 	// Set response content type
 	rw.Header().Set("Content-Type", "application/json")
 
@@ -26,43 +14,41 @@ func validateChirpHandler(rw http.ResponseWriter, rq *http.Request) {
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		log.Printf("Error decoding body: %s", err)
-		rw.WriteHeader(http.StatusInternalServerError)
+		err = respondWithError(rw, http.StatusInternalServerError, "Invalid request payload")
+		if err != nil {
+			log.Printf("Error responding: %v", err)
+		}
 		return
 	}
 
 	// Return error if chirp longer than 140 characters
 	if len(params.Body) > 140 {
-		respBody := returnError{
-			Error: "Chirp is too long",
-		}
-		data, err := json.Marshal(respBody)
+		err = respondWithError(rw, http.StatusBadRequest, "Chirp is too long")
 		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			rw.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		rw.WriteHeader(http.StatusBadRequest)
-		_, err = rw.Write(data)
-		if err != nil {
-			log.Printf("Error writing response: %s", err)
+			log.Printf("Error responding: %v", err)
 		}
 		return
 	}
 
-	// Chirp is valid
-	respBody := returnValid{
-		Valid: true,
+	// Clean the chirp
+	respBody := returnCleaned{
+		CleanedBody: replaceBadWords(params.Body),
 	}
 	data, err := json.Marshal(respBody)
 	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		rw.WriteHeader(http.StatusInternalServerError)
+		err = respondWithError(rw, http.StatusInternalServerError, "Error marshalling response")
+		if err != nil {
+			log.Printf("Error responding: %v", err)
+		}
 		return
 	}
 	rw.WriteHeader(http.StatusOK)
 	_, err = rw.Write(data)
 	if err != nil {
-		log.Printf("Error writing response: %s", err)
+		err = respondWithError(rw, http.StatusInternalServerError, "Error writing response")
+		if err != nil {
+			log.Printf("Error responding: %v", err)
+		}
+		return
 	}
 }
