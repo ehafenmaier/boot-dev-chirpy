d8f8f4e31515572f5ce567d7c02fd9f81c740004
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-10-27 17:48:41 -0700

    Update admin reset endpoint to reset users database table

--- reset.go
+++ reset.go
@@ -6,6 +6,25 @@ import (
 )
 
 func (cfg *apiConfig) resetHandler(rw http.ResponseWriter, rq *http.Request) {
+	// Return forbidden if platform is not "dev"
+	if cfg.platform != "dev" {
+		err := respondWithError(rw, http.StatusForbidden, "Forbidden")
+		if err != nil {
+			log.Printf("Error responding: %v", err)
+		}
+		return
+	}
+
+	// Reset users table
+	err := cfg.db.ResetUsers(rq.Context())
+	if err != nil {
+		err = respondWithError(rw, http.StatusInternalServerError, "Error resetting users")
+		if err != nil {
+			log.Printf("Error responding: %v", err)
+		}
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	log.Printf("Hits reset to 0")
 	rw.WriteHeader(http.StatusOK)
