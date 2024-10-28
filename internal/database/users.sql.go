d9c59f32226a1b7bc6338d32d26a49ddf343e32f
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-10-27 17:41:27 -0700

    Add sqlc function to reset users table

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -12,7 +12,6 @@ import (
 const createUser = `-- name: CreateUser :one
 INSERT INTO users (id, created_at, updated_at, email)
 VALUES (gen_random_uuid(), now(), now(), $1)
-
 RETURNING id, created_at, updated_at, email
 `
 
@@ -26,4 +25,13 @@ func (q *Queries) CreateUser(ctx context.Context, email string) (User, error) {
 		&i.Email,
 	)
 	return i, err
+}
+
+const resetUsers = `-- name: ResetUsers :exec
+DELETE FROM users
+`
+
+func (q *Queries) ResetUsers(ctx context.Context) error {
+	_, err := q.db.ExecContext(ctx, resetUsers)
+	return err
 }
