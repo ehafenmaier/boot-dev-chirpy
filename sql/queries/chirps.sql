6dd7838d4e773566cf434cff0cda01a9ce70f9be
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-10-28 22:36:19 -0700

    Add sqlc query to get chirp by id

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -6,4 +6,10 @@ RETURNING *;
 -- name: GetChirps :many
 SELECT id, created_at, updated_at, body, user_id
 FROM chirps
-ORDER BY created_at ASC;
+ORDER BY created_at;
+
+-- name: GetChirpById :one
+SELECT id, created_at, updated_at, body, user_id
+FROM chirps
+WHERE id = $1
+LIMIT 1;
