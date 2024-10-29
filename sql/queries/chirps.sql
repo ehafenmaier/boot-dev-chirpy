0908102cb6074a8eafe08ccad6fc77e8408cb45f
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-10-28 17:56:33 -0700

    Add query to get a list of chirps

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -2,3 +2,8 @@
 INSERT INTO chirps (id, created_at, updated_at, body, user_id)
 VALUES (gen_random_uuid(), now(), now(), $1, $2)
 RETURNING *;
+
+-- name: GetChirps :many
+SELECT id, created_at, updated_at, body, user_id
+FROM chirps
+ORDER BY created_at ASC;
