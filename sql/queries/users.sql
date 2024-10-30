c4dc2dbabc2605d1333e3966ecd7c5febc89d545
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-10-29 21:28:33 -0700

    Update models and queries to create user with password and get user by email

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,7 +1,13 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
-VALUES (gen_random_uuid(), now(), now(), $1)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+VALUES (gen_random_uuid(), now(), now(), $1, $2)
 RETURNING *;
 
 -- name: ResetUsers :exec
 DELETE FROM users;
+
+-- name: GetUserByEmail :one
+SELECT id, created_at, updated_at, email, hashed_password
+FROM users
+WHERE email = $1
+LIMIT 1;
