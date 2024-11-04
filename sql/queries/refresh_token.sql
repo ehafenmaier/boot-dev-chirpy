1dc3a5f00f5b997bd4ffc8448a0312c83c2995db
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-11-03 21:49:24 -0800

    Add database queries to refresh and revoke refresh tokens

--- sql/queries/refresh_token.sql
+++ sql/queries/refresh_token.sql
@@ -3,6 +3,12 @@ INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at)
 VALUES ($1, now(), now(), $2, now() + interval '60 day')
 RETURNING *;
 
+-- name: GetRefreshToken :one
+SELECT token, created_at, updated_at, user_id, expires_at, revoked_at
+FROM refresh_tokens
+WHERE token = $1
+LIMIT 1;
+
 -- name: GetUserFromRefreshToken :one
 SELECT user_id
 FROM refresh_tokens
