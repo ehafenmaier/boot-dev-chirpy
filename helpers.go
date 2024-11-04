26e68eaaef29c75d67ec068237497e0d84f9f311
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-11-04 09:33:57 -0800

    Create helper function to return a 204 no content response

--- helpers.go
+++ helpers.go
@@ -12,28 +12,29 @@ type returnError struct {
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	if payload == nil {
-		w.WriteHeader(http.StatusNoContent)
-		return nil
-	}
+	w.Header().Set("Content-Type", "application/json")
 
 	response, err := json.Marshal(payload)
 	if err != nil {
 		return err
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	_, err = w.Write(response)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) error {
 	return respondWithJSON(w, code, returnError{Error: msg})
+}
+
+func respondWithNoContent(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func replaceBadWords(body string) string {
